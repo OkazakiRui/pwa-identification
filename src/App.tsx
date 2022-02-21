d1fa10bda0471757a4599bdac7b971e8ff0a8565
RuiOkazaki <okazaki200226@gmail.com>
2022-02-21 11:07:49 +0900

    update: webとpwaで挙動を変更した

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, VFC } from 'react';
+import './App.scss';
 
-function App() {
+const App: VFC = () => {
+  const checkFC = () => {
+    const isPWA = window.matchMedia('(display-mode: standalone)').matches;
+    if (isPWA) {
+      alert('hello! PWA!');
+    } else {
+      alert('hello! WEB!');
+    }
+  };
+  useEffect(() => checkFC(), []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      WEB : red
+      <br />
+      PWA : blue
+    </>
   );
-}
+};
 
 export default App;
